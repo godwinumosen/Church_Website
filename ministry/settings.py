34b2bcec17dd8ece257659dc4c4b3f9c981a09d9
GodwinCoder568 <121322124+GodwinCoder568@users.noreply.github.com>
2023-08-07 07:58:19 -0700

    first commit to church website

--- ministry/settings.py
+++ ministry/settings.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/topics/settings/
 
 For the full list of settings and their values, see
+
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fountain',
 ]
 
 MIDDLEWARE = [
