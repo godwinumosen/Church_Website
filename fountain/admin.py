df1f7b02f7c7eec874c5581927ae88f005956b25
GodwinCoder568 <121322124+GodwinCoder568@users.noreply.github.com>
2023-08-16 02:10:14 -0700

    creating a model and the admin section

--- fountain/admin.py
+++ fountain/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import ChurchActivity
+from .models import Ministries
+from .models import Pastors
+from .models import UpcomingEvent
 
-# Register your models here.
+
+class AdminChurchActivity (admin.ModelAdmin):
+    list_display = []
+admin.site.register(ChurchActivity,AdminChurchActivity)
+
+class AdminMinistries(admin.ModelAdmin):
+    list_display = []
+admin.site.register(Ministries,AdminMinistries)
+
+class AdminPastors (admin.ModelAdmin):
+    list_display = []
+admin.site.register(Pastors, AdminPastors)
+
+class AdminUpcomingEvent (admin.ModelAdmin):
+    list_display = []
+admin.site.register(UpcomingEvent, AdminUpcomingEvent)
--- fountain/models.py
+++ fountain/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import datetime, date
 
-# Create your models here.
+class ChurchActivity (models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    img = models.ImageField(upload_to='images/')
+    publish_date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    class Meta:
+        ordering =['-publish_date']
+    
+    def __str__(self):
+        return self.title
+    
+class Pastors (models.Model):
+    pastor_name = models.CharField(max_length=250)
+    pastor_img = models.ImageField(upload_to='image/')
+    pastor_content = models.TextField()
+    
+    def __str__(self) :
+        return self.pastor_name
+    
+class Ministries (models.Model):
+    Minister_name_or_title = models.CharField(max_length=250)
+    Minister_content =  models.TextField()
+    Minister_img = models.ImageField(upload_to='imag/')
+    Minister_publish_date = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-Minister_publish_date']
+    def __str__(self):
+        return self.Minister_name_or_title
+    
+class UpcomingEvent (models.Model):
+    event_title = models.CharField(max_length=250)
+    event_content = models.TextField()
+    event_img = models.ImageField(upload_to='event_img/')
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
