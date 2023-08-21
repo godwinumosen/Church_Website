4b4eead4a4968f3f6f83a1bcea42af8ee33a5d6c
GodwinCoder568 <121322124+GodwinCoder568@users.noreply.github.com>
2023-08-21 04:15:28 -0700

    crested if settings.debug in the urls

--- ministry/settings.py
+++ ministry/settings.py
@@ -120,7 +120,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/index/'
+LOGOUT_REDIRECT_URL = '/index/'
