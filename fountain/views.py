ae64d14db96323c87dc41195bb9fe461df251596
Godwin Umosen <umosengodwin568@gmail.com>
2024-03-19 00:11:04 -0700

    Contact map api

--- fountain/views.py
+++ fountain/views.py
@@ -10,6 +10,12 @@ class HomeView(ListView):
     model = ChurchActivityModel
     template_name = 'fountain/home.html'
 
+    #This model is for the sub category of the blog
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['second_constructions'] = SecondChurchActivityModel.objects.all()
+        return context
+
 
 class ArticleDetailView(DetailView):
     model = ChurchActivityModel
@@ -19,6 +25,15 @@ class ArticleDetailView(DetailView):
         object = get_object_or_404(ChurchActivityModel, pk=pk)
         return render(request, 'article_detail.html', {'detail': object})
 
+
+class SecondChurchActivityDetailViewArticleDetailView(DetailView):
+    model = SecondChurchActivityModel
+    template_name = 'josep/second_article_detail.html'
+    context_object_name = 'second_construction'
+
+    def SecondChurchActivityDetailViewArticleDetailView(request, pk):  
+        object = get_object_or_404(SecondChurchActivityModel, pk=pk)
+        return render(request, 'josep/second_article_detail.html', {'second_detail': object})
 
 # The about page
 def about (request):
